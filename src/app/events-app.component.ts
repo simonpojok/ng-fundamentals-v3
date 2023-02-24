d510814ba36ebfdf06108880094ac06c8ff2d983
simonpojok <simonpojok@gmail.com>
2023-02-24 19:10:52 +0300

    added boostrap

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
-  template: `<h1>Hello World</h1>`,
+  selector: 'app-events',
+  template: `
+    <h1>Hello World</h1>
+    <img src="/assets/images/basic-shield.png"/>
+  `,
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';

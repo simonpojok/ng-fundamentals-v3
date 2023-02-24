7b588b1ea51236b5772b401a55df5af1b18c8caa
simonpojok <simonpojok@gmail.com>
2023-02-24 19:48:32 +0300

    created event thumbnail and communicating with the component well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import {EventsListComponent} from './events/events-list.component';
+import {EventThumbnailComponent} from './events/event-thumbnail.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   bootstrap: [EventsAppComponent]
 })

75ee49751da8b5e73589ccd99bb3f8f636964099
simonpojok <simonpojok@gmail.com>
2023-02-25 00:17:40 +0300

    fixed the online url rendering

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -11,18 +11,23 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
         .well div {
             color: #bbb
         }
+
+        .thumbnail {
+            min-height: 210px;
+        }
     `
   ],
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div>Time: {{event?.time}}</div>
+      <div>Price: \${{event?.price}}</div>
+      <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
       </div>
+      <div *ngIf="event?.onlineUrl">Online Url: {{event.onlineUrl}}</div>
     </div>
   `,
 })
--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -2,6 +2,24 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styles: [
+    `
+        .nav .navbar-nav {
+            font-size: 15px;
+        }
+
+        #searchForm {
+            margin-right: 100px;
+        }
+
+        @media (max-width: 1200px) {
+            #searchForm {
+                display: none
+            }
+        }
+
+    `
+  ]
 })
 export class NavBarComponent {}

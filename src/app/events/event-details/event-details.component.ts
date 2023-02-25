44da94865c2f087a2c084677f62c3b74a39ef3ce
simonpojok <simonpojok@gmail.com>
2023-02-25 11:43:46 +0300

    create new components

---
+++ src/app/events/event-details/event-details.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {EventService} from '../shared/event.service';
+
+@Component({
+  selector: 'app-event-details',
+  templateUrl: './event-details.component.html',
+  styles: [`
+      .container {
+          padding-left: 20px;
+          padding-right: 20px;
+      }
+
+      .event-image {
+          height: 100px;
+      }
+  `]
+})
+export class EventDetailsComponent implements OnInit {
+  event: any;
+
+  constructor(private eventService: EventService) {
+  }
+
+  ngOnInit(): void {
+    this.event = this.eventService.getEvent(1);
+  }
+}
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -5,6 +5,10 @@ export class EventService {
   getEvents(): any[] {
     return EVENTS;
   }
+
+  getEvent(id: number) {
+    return EVENTS.find(event => event.id === id);
+  }
 }
 
 const EVENTS = [

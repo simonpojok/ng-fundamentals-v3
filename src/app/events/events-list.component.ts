33df2c09e9ff2b1b9864079f3cbe3dd9bf6c1eba
simonpojok <simonpojok@gmail.com>
2023-02-25 11:17:50 +0300

    created wrapper service for toastr

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from './shared/event.service';
+import {ToastrService} from '../common/toastr.service';
+
+declare let toastr;
 
 @Component({
   selector: 'app-events-list',
@@ -9,7 +12,7 @@ import {EventService} from './shared/event.service';
       <hr>
       <div class="row">
         <div *ngFor="let event of events" class="col-md-5">
-          <app-event-thumbnail [event]="event"></app-event-thumbnail>
+          <app-event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></app-event-thumbnail>
         </div>
       </div>
     </div>
@@ -18,10 +21,14 @@ import {EventService} from './shared/event.service';
 export class EventsListComponent implements OnInit {
   events: any[];
 
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private toastrService: ToastrService) {
   }
 
   ngOnInit(): void {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName: string) {
+    this.toastrService.success(eventName);
   }
 }
